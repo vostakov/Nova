96be5e08d86f11c31e9b5af1e914a11d5a2a8533
vlad <Dolchvladf@gmail.com>
2024-10-22 11:35:31 +0300

    2nd try
    HELL YEAH

--- nova.py
+++ nova.py
@@ -1,6 +1,13 @@
 import cianparser
+import pandas as ps
 
 moscow_parser = cianparser.CianParser(location="Саратов")
-data = moscow_parser.get_flats(deal_type="sale", rooms=(1, 2), additional_settings={"start_page":1, "end_page":2})
+data = moscow_parser.get_flats(deal_type="sale", rooms=(1, "studio"), additional_settings={"start_page":1, "end_page":5})
+
+sorted_price = sorted(data, key=lambda x: x[ 'price'])
+
+table = ps.DataFrame(data)
+
+table.to_csv('cian_parsing.csv', index = False)
 
 print(data)
