a7e82fa67057d14006ef35e623502c5653a343ab
vostakov <72863270+vostakov@users.noreply.github.com>
2024-10-29 12:16:06 +0300

    6k 6k
    6k

--- nova.py
+++ nova.py
@@ -1,18 +1,12 @@
 import cianparser
 import pandas as ps
-import time
-#import random
 
-moscow_parser = cianparser.CianParser(location="Королёв")
-data = []
-for i in range(25, 30):
-    a = moscow_parser.get_flats(deal_type="sale", rooms=(1, "studio", 2, 3, 4, 5), additional_settings={"start_page":i, "end_page":i})
-    data.extend(a)
-    time.sleep(2)
+parser = cianparser.CianParser(location="икша")
+
+data = parser.get_flats(deal_type="sale", rooms=(1, 2), additional_settings={"start_page": 12, "end_page": 13}, with_extra_data=True)
 
 table = ps.DataFrame(data)
+spisok = table
+spisok.to_csv('zxc.csv', mode='a', header = False, index = False)
 
-columbs = ['author', 'price', 'deal_type', 'location', 'floors_count', 'rooms_count', 'total_meters', 'accommodation_type', 'district', 'street', 'house_number', 'underground', 'residential_complex']
-spisok = table[columbs]
-spisok.to_csv('cian_parsing.csv', mode='a', header = False, index = False)
 print(spisok)
