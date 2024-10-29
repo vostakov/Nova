bdeb363083ef74577fe496f4a8c50a399917e573
vostakov <72863270+vostakov@users.noreply.github.com>
2024-10-29 22:14:27 +0300

    iksha
    52

--- nova.py
+++ nova.py
@@ -3,7 +3,7 @@ import pandas as ps
 
 parser = cianparser.CianParser(location="икша")
 
-data = parser.get_flats(deal_type="sale", rooms=(1, 2), additional_settings={"start_page": 14, "end_page": 16}, with_extra_data=True)
+data = parser.get_flats(deal_type="sale", rooms=(3, 4, 5), additional_settings={"start_page": 41, "end_page": 43}, with_extra_data=True)
 
 table = ps.DataFrame(data)
 spisok = table
