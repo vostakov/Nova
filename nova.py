164c9aaf905a6afdd4465633a986d725788e556b
deni1gugu <72863270+vostakov@users.noreply.github.com>
2024-10-30 19:27:47 +0300

    pre-final
    i'm tired

--- nova.py
+++ nova.py
@@ -3,10 +3,10 @@ import pandas as ps
 
 parser = cianparser.CianParser(location="икша")
 
-data = parser.get_flats(deal_type="sale", rooms=(3, 4, 5), additional_settings={"start_page": 41, "end_page": 43}, with_extra_data=True)
+data = parser.get_flats(deal_type="sale", rooms=(fddf), additional_settings={"start_page": 1, "end_page": 1}, with_extra_data=True)
 
 table = ps.DataFrame(data)
 spisok = table
-spisok.to_csv('zxc.csv', mode='a', header = False, index = False)
+spisok.to_csv('zyc.csv', mode='a', header = False, index = False)
 
 print(spisok)
